21470d922c229c85f7be312c55cca246a876c24e
scoorp29 <scorp31.scorp31@gmail.com>
2019-02-12 09:31:04 +0100

    Interface Admin

---
+++ src/Form/AddEventType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Event;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+
+class AddEventType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('picture')
+            ->add('startDate', DateType::class,[
+                'widget' => 'single_text',
+                //'html5' => false,
+            ])
+            ->add('endDate', DateType::class,[
+                'widget' => 'single_text',
+                //'html5' => false,
+            ])
+            ->add('description', TextareaType::class, array('required' => false))
+            ->add('submit', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Event::class,
+        ]);
+    }
+}
